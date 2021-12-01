3f53d4600a633945940753f5b33135a7c21aff99
AlecMoore <alec.moore47@yahoo.com>
2021-12-01 21:13:34 +0000

    Initial commit

---
+++ src/main/java/SimpleBuySellBot/Config.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SimpleBuySellBot;
+
+/**
+ *
+ * @author Alec
+ */
+public class Config {
+    float dipThreshold = 2;
+    //float upwardTrendThreshold;
+    float profitThreshold = 2;
+    //float stopLossThreshold;
+    
+    
+}
